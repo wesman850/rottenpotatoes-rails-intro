869e0ad0e85c7142ab79ae8d58ec4eb3d0b4c00a
wesleyng <wesleyng@tamu.edu>
2018-02-16 00:21:14 -0600

    Adding all files after finishing parts

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,59 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    # @movies = Movie.all
+    @all_ratings = Movie.allRatings
+    #Part 1 Code
+    # if params[:ratings]
+    #   @movies = Movie.where(:rating => params[:ratings].keys)
+    # else
+    #   @movies = Movie.all
+    # end
+    session[:ratings] || session[:ratings] = Hash[@all_ratings.map {|rating| [rating, rating]}]
+    session[:sort] || session[:sort] = 'id'
+
+    if params[:ratings]
+      temp = params[:ratings]
+      session[:ratings] = temp
+    end
+
+    if params[:sort]
+      temp = params[:sort]
+      session[:sort] = temp
+    end
+
+    heuristic1 = !params[:ratings]
+    heuristic2 = !params[:sort]
+
+    #Check Heuristic
+    if(heuristic1 || heuristic2)
+      #No matches
+      flash.keep
+      #Redirection
+      redirect_to :ratings => session[:ratings], :sort => session[:sort]
+    end
+
+     @movies = Movie.where(:rating => session[:ratings].keys).order(session[:sort])
+
+
+
+    #PART 2
+    # :rate => params[:ratings].keys
+    # if params[:ratings].present?
+    #   @ratings_picked = params[:ratings]
+    # else
+    #   @ratings_picked = []
+    # end
+    # Checking to see if movie titles are in order
+
+    #PART 1
+    # if params[:sortTitleFunction] == "TitleSort"
+    #   @movies = @movies.order(:title)
+    # end
+    # #Checking to see if movie release dates are in order
+    # if params[:sortReleaseFunction] == "ReleaseSort"
+    #   @movies = @movies.order(:release_date)
+    # end
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+    def self.allRatings
+        self.select(:rating).map{|x| x.rating}.sort.uniq
+    end
+    def self.fourRatings
+        ['G','PG','PG-13','R']
+    end
 end
